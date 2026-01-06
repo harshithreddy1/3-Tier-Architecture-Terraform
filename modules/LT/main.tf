788f1201eb41050df8137d1f6a9b03afea1bce50
harshithreddy1 <127827837+harshithreddy1@users.noreply.github.com>
2026-01-07 00:21:26 +0530

    Update main.tf

--- modules/LT/main.tf
+++ modules/LT/main.tf
@@ -6,6 +6,10 @@ resource "aws_launch_template" "Frontend_LT" {
     key_name = var.key_pair_name
     vpc_security_group_ids = [var.Frontend_LT_SG]
     user_data = base64encode(file("${path.module}/pub_userdata.sh"))
+# Base64encode converts data into a safe text format so it can be transmitted and processed without errors. EC2 API accepts user data only in Base64 format.
+# file("   ") reads the content of a file and returns it as a string to Terraform.
+# $ is used to replace a variable or expression with its actual value inside a string.
+# {path.module} gives the absolute path of the current Terraform module directory.
 
     tag_specifications {
       resource_type = var.LT_resource_type
