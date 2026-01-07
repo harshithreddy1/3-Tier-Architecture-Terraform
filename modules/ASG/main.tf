cdd491a6c76997c90522ba1c4ed6bbc1d74077e7
harshithreddy1 <127827837+harshithreddy1@users.noreply.github.com>
2026-01-07 11:40:59 +0530

    Update main.tf

--- modules/ASG/main.tf
+++ modules/ASG/main.tf
@@ -4,7 +4,10 @@ resource "aws_autoscaling_group" "Frontend_ASG" {
     max_size = var.max_size
     desired_capacity = var.desired_capacity
     vpc_zone_identifier = var.pub_subnets
+# defines the subnets and Availability Zones where ASG should launch instances.
     health_check_type = var.health_check_type
+# ELB health check verifies that the application is actually working, not just that EC2 is running.
+# this ensures ASG to replace instances when the application fails, not just when EC2 stops.
     target_group_arns = [var.Frontend_TG_arn]
 
     launch_template {
