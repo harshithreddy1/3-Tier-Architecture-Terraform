45f210d60fb439087c702df7348fc33fdb17e66f
harshithreddy1 <127827837+harshithreddy1@users.noreply.github.com>
2026-01-07 12:29:18 +0530

    Update main.tf

--- modules/RDS/main.tf
+++ modules/RDS/main.tf
@@ -9,6 +9,7 @@ resource "aws_db_instance" "rds" {
     engine = var.rds_engine
     engine_version = var.rds_engine_version
     instance_class = var.rds_instance_class
+# Defines the size and power of the RDS database instance, like cpu & RAM (memory)
     publicly_accessible = var.rds_publicly_accessible
     username = var.username
     password = var.password
@@ -16,6 +17,9 @@ resource "aws_db_instance" "rds" {
     db_subnet_group_name = aws_db_subnet_group.db_subnet_group.name
     vpc_security_group_ids = [var.rds_sg]
     skip_final_snapshot = var.rds_skip_final_snapshot
+# Controls whether AWS takes a final backup before deleting the RDS instance.
     multi_az = var.rds_multi_az
+# Enables high availability by creating a standby database in another Availability Zone.
     storage_type = var.rds_storage_type
 }
+# Storage type defines the kind of disk used by the RDS database and its performance.
